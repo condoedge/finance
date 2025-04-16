c65bc00168d4400007e61e2dbbdf748e8657800d
Ezequiel <tavelliezequiel@gmail.com>
2025-04-16 12:22:18 -0300

    Minor temp fixes

--- src/Helpers/finance.php
+++ src/Helpers/finance.php
@@ -1,12 +1,5 @@
 <?php
 
-function _TotalCurrencyCols($title, $id, $amount = 0, $border = true)
-{
-    return _Columns(
-        _Html($title)->class('text-level1 font-medium'),
-        _Currency($amount)->id($id)->class('ccy-amount text-lg text-level1')
-    )->class('px-4 py-2 text-right font-semibold'.($border ? '': ' -mt-4'));
-}
 
 function _TaxesInfoLink()
 {
