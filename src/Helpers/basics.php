3fbb6eda83667eea96112e89cd6664ebb3c806ab
Ezequiel <tavelliezequiel@gmail.com>
2025-07-27 16:20:15 -0300

    fix: using another way to sanatize string without transliterator if extension is not installed

--- src/Helpers/basics.php
+++ src/Helpers/basics.php
@@ -14,79 +14,93 @@ if (!function_exists('integerArray')) {
 if (!function_exists('sanitizeString')) {
     function sanitizeString(string $value): string
     {
-        try {
-            $transliterator = Transliterator::createFromRules(
-                ':: NFD; :: [:Nonspacing Mark:] Remove; :: NFC;',
-                Transliterator::FORWARD
-            );
+        // Check if Transliterator class exists (requires php-intl extension)
+        if (class_exists('Transliterator')) {
+            try {
+                $transliterator = \Transliterator::createFromRules(
+                    ':: NFD; :: [:Nonspacing Mark:] Remove; :: NFC;',
+                    \Transliterator::FORWARD
+                );
 
-            $value = $transliterator->transliterate($value);
-        } catch (\Exception $e) {
-            // Remove accents using character mapping
-            $accents = [
-                'À' => 'A',
-                'Á' => 'A',
-                'Â' => 'A',
-                'Ã' => 'A',
-                'Ä' => 'A',
-                'Å' => 'A',
-                'à' => 'a',
-                'á' => 'a',
-                'â' => 'a',
-                'ã' => 'a',
-                'ä' => 'a',
-                'å' => 'a',
-                'È' => 'E',
-                'É' => 'E',
-                'Ê' => 'E',
-                'Ë' => 'E',
-                'è' => 'e',
-                'é' => 'e',
-                'ê' => 'e',
-                'ë' => 'e',
-                'Ì' => 'I',
-                'Í' => 'I',
-                'Î' => 'I',
-                'Ï' => 'I',
-                'ì' => 'i',
-                'í' => 'i',
-                'î' => 'i',
-                'ï' => 'i',
-                'Ò' => 'O',
-                'Ó' => 'O',
-                'Ô' => 'O',
-                'Õ' => 'O',
-                'Ö' => 'O',
-                'ò' => 'o',
-                'ó' => 'o',
-                'ô' => 'o',
-                'õ' => 'o',
-                'ö' => 'o',
-                'Ù' => 'U',
-                'Ú' => 'U',
-                'Û' => 'U',
-                'Ü' => 'U',
-                'ù' => 'u',
-                'ú' => 'u',
-                'û' => 'u',
-                'ü' => 'u',
-                'Ý' => 'Y',
-                'ý' => 'y',
-                'ÿ' => 'y',
-                'Ñ' => 'N',
-                'ñ' => 'n',
-                'Ç' => 'C',
-                'ç' => 'c',
-                'Æ' => 'AE',
-                'æ' => 'ae',
-                'Œ' => 'OE',
-                'œ' => 'oe'
-            ];
-
-            $value = strtr($value, $accents);
+                $value = $transliterator->transliterate($value);
+            } catch (\Exception $e) {
+                // Fallback to manual mapping if transliterator fails
+                $value = removeAccentsManually($value);
+            }
+        } else {
+            // Fallback to manual mapping if Transliterator class doesn't exist
+            $value = removeAccentsManually($value);
         }
 
         // Remove pipe characters and trim
         return trim(str_replace('|', '', $value));
+    }
+}
+
+if (!function_exists('removeAccentsManually')) {
+    function removeAccentsManually(string $value): string
+    {
+        // Remove accents using character mapping
+        $accents = [
+            'À' => 'A',
+            'Á' => 'A',
+            'Â' => 'A',
+            'Ã' => 'A',
+            'Ä' => 'A',
+            'Å' => 'A',
+            'à' => 'a',
+            'á' => 'a',
+            'â' => 'a',
+            'ã' => 'a',
+            'ä' => 'a',
+            'å' => 'a',
+            'È' => 'E',
+            'É' => 'E',
+            'Ê' => 'E',
+            'Ë' => 'E',
+            'è' => 'e',
+            'é' => 'e',
+            'ê' => 'e',
+            'ë' => 'e',
+            'Ì' => 'I',
+            'Í' => 'I',
+            'Î' => 'I',
+            'Ï' => 'I',
+            'ì' => 'i',
+            'í' => 'i',
+            'î' => 'i',
+            'ï' => 'i',
+            'Ò' => 'O',
+            'Ó' => 'O',
+            'Ô' => 'O',
+            'Õ' => 'O',
+            'Ö' => 'O',
+            'ò' => 'o',
+            'ó' => 'o',
+            'ô' => 'o',
+            'õ' => 'o',
+            'ö' => 'o',
+            'Ù' => 'U',
+            'Ú' => 'U',
+            'Û' => 'U',
+            'Ü' => 'U',
+            'ù' => 'u',
+            'ú' => 'u',
+            'û' => 'u',
+            'ü' => 'u',
+            'Ý' => 'Y',
+            'ý' => 'y',
+            'ÿ' => 'y',
+            'Ñ' => 'N',
+            'ñ' => 'n',
+            'Ç' => 'C',
+            'ç' => 'c',
+            'Æ' => 'AE',
+            'æ' => 'ae',
+            'Œ' => 'OE',
+            'œ' => 'oe'
+        ];
+
+        return strtr($value, $accents);
     }
 }
