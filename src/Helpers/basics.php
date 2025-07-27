adc695b77ef2605934d083b12b615501e61e0b7a
Ezequiel <tavelliezequiel@gmail.com>
2025-07-27 16:08:20 -0300

    fix: using another way to sanatize string without transliterator if extension is not installed

--- src/Helpers/basics.php
+++ src/Helpers/basics.php
@@ -14,13 +14,77 @@ if (!function_exists('integerArray')) {
 if (!function_exists('sanitizeString')) {
     function sanitizeString(string $value): string
     {
-        // Remove accents
-        $transliterator = Transliterator::createFromRules(
-            ':: NFD; :: [:Nonspacing Mark:] Remove; :: NFC;',
-            Transliterator::FORWARD
-        );
+        try {
+            $transliterator = Transliterator::createFromRules(
+                ':: NFD; :: [:Nonspacing Mark:] Remove; :: NFC;',
+                Transliterator::FORWARD
+            );
 
-        $value = $transliterator->transliterate($value);
+            $value = $transliterator->transliterate($value);
+        } catch (\Exception $e) {
+            // Remove accents using character mapping
+            $accents = [
+                'À' => 'A',
+                'Á' => 'A',
+                'Â' => 'A',
+                'Ã' => 'A',
+                'Ä' => 'A',
+                'Å' => 'A',
+                'à' => 'a',
+                'á' => 'a',
+                'â' => 'a',
+                'ã' => 'a',
+                'ä' => 'a',
+                'å' => 'a',
+                'È' => 'E',
+                'É' => 'E',
+                'Ê' => 'E',
+                'Ë' => 'E',
+                'è' => 'e',
+                'é' => 'e',
+                'ê' => 'e',
+                'ë' => 'e',
+                'Ì' => 'I',
+                'Í' => 'I',
+                'Î' => 'I',
+                'Ï' => 'I',
+                'ì' => 'i',
+                'í' => 'i',
+                'î' => 'i',
+                'ï' => 'i',
+                'Ò' => 'O',
+                'Ó' => 'O',
+                'Ô' => 'O',
+                'Õ' => 'O',
+                'Ö' => 'O',
+                'ò' => 'o',
+                'ó' => 'o',
+                'ô' => 'o',
+                'õ' => 'o',
+                'ö' => 'o',
+                'Ù' => 'U',
+                'Ú' => 'U',
+                'Û' => 'U',
+                'Ü' => 'U',
+                'ù' => 'u',
+                'ú' => 'u',
+                'û' => 'u',
+                'ü' => 'u',
+                'Ý' => 'Y',
+                'ý' => 'y',
+                'ÿ' => 'y',
+                'Ñ' => 'N',
+                'ñ' => 'n',
+                'Ç' => 'C',
+                'ç' => 'c',
+                'Æ' => 'AE',
+                'æ' => 'ae',
+                'Œ' => 'OE',
+                'œ' => 'oe'
+            ];
+
+            $value = strtr($value, $accents);
+        }
 
         // Remove pipe characters and trim
         return trim(str_replace('|', '', $value));
