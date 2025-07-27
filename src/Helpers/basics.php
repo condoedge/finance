0c8416d094a3f87cb121433b83b255f1c166442b
Ezequiel <tavelliezequiel@gmail.com>
2025-07-27 16:30:13 -0300

    fix: using another way to sanatize string without transliterator if extension is not installed

--- src/Helpers/basics.php
+++ src/Helpers/basics.php
@@ -40,67 +40,75 @@ if (!function_exists('sanitizeString')) {
 if (!function_exists('removeAccentsManually')) {
     function removeAccentsManually(string $value): string
     {
-        // Remove accents using character mapping
+        // Remove accents using comprehensive character mapping
         $accents = [
-            'À' => 'A',
-            'Á' => 'A',
-            'Â' => 'A',
-            'Ã' => 'A',
-            'Ä' => 'A',
-            'Å' => 'A',
-            'à' => 'a',
-            'á' => 'a',
-            'â' => 'a',
-            'ã' => 'a',
-            'ä' => 'a',
-            'å' => 'a',
-            'È' => 'E',
-            'É' => 'E',
-            'Ê' => 'E',
-            'Ë' => 'E',
-            'è' => 'e',
-            'é' => 'e',
-            'ê' => 'e',
-            'ë' => 'e',
-            'Ì' => 'I',
-            'Í' => 'I',
-            'Î' => 'I',
-            'Ï' => 'I',
-            'ì' => 'i',
-            'í' => 'i',
-            'î' => 'i',
-            'ï' => 'i',
-            'Ò' => 'O',
-            'Ó' => 'O',
-            'Ô' => 'O',
-            'Õ' => 'O',
-            'Ö' => 'O',
-            'ò' => 'o',
-            'ó' => 'o',
-            'ô' => 'o',
-            'õ' => 'o',
-            'ö' => 'o',
-            'Ù' => 'U',
-            'Ú' => 'U',
-            'Û' => 'U',
-            'Ü' => 'U',
-            'ù' => 'u',
-            'ú' => 'u',
-            'û' => 'u',
-            'ü' => 'u',
-            'Ý' => 'Y',
-            'ý' => 'y',
-            'ÿ' => 'y',
-            'Ñ' => 'N',
-            'ñ' => 'n',
-            'Ç' => 'C',
-            'ç' => 'c',
-            'Æ' => 'AE',
-            'æ' => 'ae',
-            'Œ' => 'OE',
-            'œ' => 'oe'
+            // Latin A
+            'À' => 'A', 'Á' => 'A', 'Â' => 'A', 'Ã' => 'A', 'Ä' => 'A', 'Å' => 'A', 'Ā' => 'A', 'Ą' => 'A', 'Ă' => 'A',
+            'à' => 'a', 'á' => 'a', 'â' => 'a', 'ã' => 'a', 'ä' => 'a', 'å' => 'a', 'ā' => 'a', 'ą' => 'a', 'ă' => 'a',
+            
+            // Latin E
+            'È' => 'E', 'É' => 'E', 'Ê' => 'E', 'Ë' => 'E', 'Ē' => 'E', 'Ę' => 'E', 'Ě' => 'E', 'Ė' => 'E', 'Ĕ' => 'E',
+            'è' => 'e', 'é' => 'e', 'ê' => 'e', 'ë' => 'e', 'ē' => 'e', 'ę' => 'e', 'ě' => 'e', 'ė' => 'e', 'ĕ' => 'e',
+            
+            // Latin I
+            'Ì' => 'I', 'Í' => 'I', 'Î' => 'I', 'Ï' => 'I', 'Ī' => 'I', 'Ĩ' => 'I', 'Ĭ' => 'I', 'Į' => 'I', 'İ' => 'I',
+            'ì' => 'i', 'í' => 'i', 'î' => 'i', 'ï' => 'i', 'ī' => 'i', 'ĩ' => 'i', 'ĭ' => 'i', 'į' => 'i', 'ı' => 'i',
+            
+            // Latin O
+            'Ò' => 'O', 'Ó' => 'O', 'Ô' => 'O', 'Õ' => 'O', 'Ö' => 'O', 'Ø' => 'O', 'Ō' => 'O', 'Ő' => 'O', 'Ŏ' => 'O',
+            'ò' => 'o', 'ó' => 'o', 'ô' => 'o', 'õ' => 'o', 'ö' => 'o', 'ø' => 'o', 'ō' => 'o', 'ő' => 'o', 'ŏ' => 'o',
+            
+            // Latin U  
+            'Ù' => 'U', 'Ú' => 'U', 'Û' => 'U', 'Ü' => 'U', 'Ū' => 'U', 'Ũ' => 'U', 'Ŭ' => 'U', 'Ů' => 'U', 'Ű' => 'U', 'Ų' => 'U',
+            'ù' => 'u', 'ú' => 'u', 'û' => 'u', 'ü' => 'u', 'ū' => 'u', 'ũ' => 'u', 'ŭ' => 'u', 'ů' => 'u', 'ű' => 'u', 'ų' => 'u',
+            
+            // Latin Y
+            'Ý' => 'Y', 'Ÿ' => 'Y', 'Ŷ' => 'Y',
+            'ý' => 'y', 'ÿ' => 'y', 'ŷ' => 'y',
+            
+            // Other Latin
+            'Ñ' => 'N', 'ñ' => 'n', 'Ń' => 'N', 'ń' => 'n', 'Ň' => 'N', 'ň' => 'n', 'Ņ' => 'N', 'ņ' => 'n',
+            'Ç' => 'C', 'ç' => 'c', 'Ć' => 'C', 'ć' => 'c', 'Ĉ' => 'C', 'ĉ' => 'c', 'Ċ' => 'C', 'ċ' => 'c', 'Č' => 'C', 'č' => 'c',
+            'Ś' => 'S', 'ś' => 's', 'Ŝ' => 'S', 'ŝ' => 's', 'Ş' => 'S', 'ş' => 's', 'Š' => 'S', 'š' => 's',
+            'Ž' => 'Z', 'ž' => 'z', 'Ź' => 'Z', 'ź' => 'z', 'Ż' => 'Z', 'ż' => 'z',
+            'Ð' => 'D', 'ð' => 'd', 'Đ' => 'D', 'đ' => 'd',
+            'Þ' => 'TH', 'þ' => 'th',
+            'ß' => 'ss',
+            'Ł' => 'L', 'ł' => 'l',
+            'Ŕ' => 'R', 'ŕ' => 'r', 'Ř' => 'R', 'ř' => 'r',
+            'Ţ' => 'T', 'ţ' => 't', 'Ť' => 'T', 'ť' => 't',
+            'Ŵ' => 'W', 'ŵ' => 'w',
+            
+            // Ligatures
+            'Æ' => 'AE', 'æ' => 'ae',
+            'Œ' => 'OE', 'œ' => 'oe',
+            'Ĳ' => 'IJ', 'ĳ' => 'ij',
         ];
 
-        return strtr($value, $accents);
+        // Special characters mapping
+        $specialChars = [
+            // Quote marks
+            chr(226).chr(128).chr(152) => "'", // left single quote
+            chr(226).chr(128).chr(153) => "'", // right single quote  
+            chr(226).chr(128).chr(156) => '"', // left double quote
+            chr(226).chr(128).chr(157) => '"', // right double quote
+            // Dashes
+            chr(226).chr(128).chr(147) => '-', // em dash
+            chr(226).chr(128).chr(148) => '-', // en dash
+            // Others
+            chr(194).chr(160) => ' ', // non-breaking space
+            chr(194).chr(173) => '', // soft hyphen
+        ];
+
+        // First, normalize using the mappings
+        $value = strtr($value, array_merge($accents, $specialChars));
+        
+        // Additional cleanup: remove any character that is not letter, number, space, hyphen, apostrophe, period
+        $value = preg_replace('/[^\p{L}\p{N}\s\-\'.]/u', '', $value);
+        
+        // Normalize multiple spaces to single space
+        $value = preg_replace('/\s+/', ' ', $value);
+        
+        return $value;
     }
 }
